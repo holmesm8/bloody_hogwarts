9a78e76bca2bf13f029bb5fd382861c533e38530
Matt Holmes <holmesm8@gmail.com>
2020-02-13 14:28:00 -0700

    Finish user story 4

--- spec/features/students/id_spec.rb
+++ spec/features/students/id_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "students id page", type: :feature do
   context "as a visitor" do
     it "can see a list of the student's courses" do
-      student1 = Student.create!(name: "Matt", age: 30, house: "Ravenclaw")
-      course1 = student1.courses.create!(name: "Defense Against the Dark Arts")
+      @student1 = Student.create!(name: "Matt", age: 30, house: "Ravenclaw")
+      @course1 = @student1.courses.create!(name: "Defense Against the Dark Arts")
 
-      visit "/students/#{student1.id}"
+      visit "/students/#{@student1.id}"
 
-      expect(page).to have_content(course1.name)
+      expect(page).to have_content(@course1.name)
     end
   end
 end
